e2dfbcd655e6b7664a4a5ea328928d723f14f581
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-08 01:47:38 -0500

    Cadenas de conexión hacia BD y proyectos secundarios

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Persistencia.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var connectionString = builder.Configuration.GetConnectionString("Conexion");
 
+builder.Services.AddDbContext<ApplicationDbContext>(
+                    opciones => { opciones.UseSqlServer( connectionString ); }
+                 );
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
